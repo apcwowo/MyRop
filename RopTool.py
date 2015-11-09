fb237654b41dddb04a5141548a74c189a1171880
zhaoxiaoqiang <zxq_yx_007@163.com>
2015-11-09 18:09:07 +0800

    classify gadgets and regitster (read or write) -- stage1

--- RopTool.py
+++ RopTool.py
@@ -3,10 +3,36 @@ from capstone import *
 from gadget import *
 from loaders.elf import *
 from struct import unpack
+from capstone.arm_const import *
+
+
+class GType:
+    GT_Move_Reg = 1     # mov r0, r2
+    GT_Arithm_Const = 2 # add r1, #1
+    GT_Arithm_Reg = 3   # add r1, r1, r2
+    GT_Load_Mem = 4     # ldr r0, [r2, #4]
+    GT_Store_Mem = 5    # str r3, [r0, #8]
+    GT_Load_Const = 6   # pop {r0, r4, lr}
+    GT_Syscall = 7      # svc 0x0
+    GT_Jump = 8         # blx, bx
+    GT_Inv = 9          # other insn
+
 
 class RopTool:
     def __init__(self, path):
         self.gadgets = []
+        self.typeDict = {
+            GType.GT_Move_Reg : [],
+            GType.GT_Arithm_Const : [],
+            GType.GT_Arithm_Reg : [],
+            GType.GT_Load_Mem : [],
+            GType.GT_Store_Mem : [],
+            GType.GT_Load_Const: [],
+            GType.GT_Syscall : [],
+            GType.GT_Jump : [],
+            GType.GT_Inv : [],
+            }
+
         self.__binary = None
         with open(path, mode='rb') as file:
             self.__binary = file.read()
@@ -19,7 +45,62 @@ class RopTool:
         for s in sections:
             self.gadgets.append(g.getROPGadgets(s))
 
-        self.output()
+        #self.output()
+        self.classifyGadgets()
+        #self.print_gadget_class()
+
+
+    def print_gadget_class(self):
+
+        for i in self.typeDict.keys():
+            if i == GType.GT_Inv:
+                continue
+
+            print "##################################"
+            print "type: %d" %(i)
+            for j in self.typeDict[i]:
+                print j["gadget"],
+                print self.regs_rw(j)
+
+
+    def GadgetType(self, node):
+        # syscall type
+        if "svc" in node["gadget"]:
+            return GType.GT_Syscall
+
+        for opcode in node["insn"]:
+            if opcode.mnemonic == "svc":
+                return GType.GT_Syscall
+            elif opcode.mnemonic == "pop":
+                return GType.GT_Load_Const
+            elif opcode.mnemonic == "ldr":
+                return GType.GT_Load_Mem
+            elif opcode.mnemonic == "str":
+                return GType.GT_Store_Mem
+            elif opcode.mnemonic == "mov":
+                if opcode.op_count(ARM_OP_REG) == 2:
+                    return GType.GT_Move_Reg
+                else:
+                    return GType.GT_Inv
+            elif opcode.mnemonic == "add":
+                #print "op == add"
+                #print opcode.op_str
+                reg_num = opcode.op_count(ARM_OP_REG)
+                imm_num = opcode.op_count(ARM_OP_IMM)
+                if reg_num >= 3:
+                    return GType.GT_Arithm_Reg
+                elif (reg_num == 2 or reg_num == 1) and imm_num == 1:
+                    return GType.GT_Arithm_Const
+            elif opcode.mnemonic == "blx" or opcode.mnemonic == "bx":
+                return GType.GT_Jump
+            else:
+                return GType.GT_Inv
+
+    def classifyGadgets(self):
+        for i in self.gadgets:
+            for j in i:
+                Gtype = self.GadgetType(j)
+                self.typeDict[Gtype].append(j)
 
     def output(self):
         for i in self.gadgets:
@@ -49,7 +130,42 @@ class RopTool:
         for i in ret:
             print "0x%x" %(i),
         print ""
-                
-            
 
+    def regs_rw(self, node):
+        ret = {"read": [],
+                "write": []
+                }
         
+        for ins in node["insn"]:
+            regs = ins.op_str.replace(' ','').split(',')
+            if ins.mnemonic == "mov":
+                if len(regs) == 2:
+                    if not regs[1] in ret:
+                        ret["read"].append(regs[1])
+                    if not regs[0] in ret:
+                        ret["write"].append(regs[0])
+
+            elif ins.mnemonic == "add":
+                ret["write"].append(regs[0])
+                ret["read"].append(regs[1])
+                if len(regs) == 3:
+                    ret["read"].append(regs[2])
+            elif ins.mnemonic == "ldr":
+                ret["write"].append(regs[0])
+                ret["read"].append(regs[1])
+            elif ins.mnemonic == "str":
+                ret["write"].append(regs[1])
+                ret["read"].append(regs[0])
+            elif ins.mnemonic == "pop":
+                for r in regs:
+                    r = r.replace('{', '')
+                    r = r.replace('}', '')
+                    ret["write"].append(r)
+            elif ins.mnemonic == "blx" or ins.mnemonic == "bx":
+                ret["read"].append(regs[0])
+            elif ins.mnemonic == "orr":
+                ret["write"].append(regs[0])
+                ret["read"].append(regs[1])
+                ret["read"].append(regs[2])
+        return ret
+
--- gadget.py
+++ gadget.py
@@ -3,6 +3,20 @@ import re
 from capstone import *
 import sys
 
+gadget_arm = [
+    [b"[\x30-\x3f]\xff\x2f\xe1", 4, 4], # blx, arm mode
+    [b"[\x10-\x1f]\xff\x2f\xe1", 4, 4], # bx, arm mode
+    [b"[\x00-\xff]\x80\xbd\xe8", 4, 4], # pop {,pc}, arm mode
+    [b"[\x00-\xff]{3}\xef", 4, 4],      # swi, arm mode
+]
+gadget_thumb = [
+    [b"[\x80-\xf8]\x47", 2, 2],         # blx, thumb mode
+    [b"[\x00-\x78]\x47", 2, 2],         # bx, thumb mode
+    [b"[\x00-\xff]\xbd", 2, 2],         # pop {,pc}, thumb mode
+    [b"[\x00-\xff]\xef", 2, 2],         # swi, thumb mode
+]
+
+
 class Gadget:
     def __init__(self, binary, depth, mode):
         self.__binary = binary
@@ -13,6 +27,7 @@ class Gadget:
     def getROPGadgets(self, section):
         arch = self.__binary.getArch()
         archMode = self.__binary.getArchMode()
+        gadgets = []
         if arch == CS_ARCH_X86:
             gadgets = [
                 [b"\xc3", 1, 1], # ret
@@ -20,19 +35,11 @@ class Gadget:
             ]
         elif arch == CS_ARCH_ARM:
             if self.__mode == "arm":
-                gadgets = [
-                    [b"[\x30-\x3f]\xff\x2f\xe1", 4, 4], # blx, arm mode
-                    [b"[\x10-\x1f]\xff\x2f\xe1", 4, 4], # bx, arm mode
-                    [b"[\x00-\xff]\x80\xbd\xe8", 4, 4], # pop {,pc}, arm mode
-                    [b"[\x00-\xff]{3}\xef", 4, 4],      # swi, arm mode
-                    ]
+                gadgets.extend(gadget_arm)
+                gadgets.extend(gadget_thumb)
+
             elif self.__mode == "thumb":
-                gadgets = [
-                    [b"[\x80-\xf8]\x47", 2, 2],         # blx, thumb mode
-                    [b"[\x00-\x78]\x47", 2, 2],         # bx, thumb mode
-                    [b"[\x00-\xff]\xbd", 2, 2],         # pop {,pc}, thumb mode
-                    [b"[\x00-\xff]\xef", 2, 2],         # swi, thumb mode
-                ]
+                gadgets.extend(gadget_thumb)
             else:
                 print "Error: Bad arm mode"
                 sys.exit(-1)
@@ -48,6 +55,8 @@ class Gadget:
         ret = []
 
         md = Cs(arch, archMode)
+        md.detail = True
+        md.syntax = CS_OPT_SYNTAX_ATT
         for gad in gadgets:
             allPos = [ m.start() for m in re.finditer(gad[OP], section["opcodes"])]
             for pos in allPos:
@@ -55,12 +64,15 @@ class Gadget:
                     if (section["vaddr"] + pos - (i * gad[ALIGN])) % gad[ALIGN] == 0: # align right
                         asms = md.disasm(section["opcodes"][pos - (i * gad[ALIGN]):pos + gad[SIZE]], section["vaddr"] + pos)
                         gadget = ""
+                        insns = []
                         for asm in asms:
+                            insns.append(asm)
                             gadget += (asm.mnemonic + " " + asm.op_str + " ; ").replace("  ", " ")
+                            #print asm.insn_name()
                         if len(gadget) > 0:
                             ret += [ {"vaddr": section["vaddr"] + pos - (i * gad[ALIGN]),
                                       "gadget": gadget,
-                                      "codes": section["opcodes"][pos - (i * gad[ALIGN]): pos + gad[SIZE]]
-                                } ]
+                                      "codes": section["opcodes"][pos - (i * gad[ALIGN]): pos + gad[SIZE]],
+                                      "insn": insns} ]
 
         return ret
